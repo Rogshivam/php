3660de24d2ff79e1594ae5c2a7eb11c27676f1ec
shivam <shivamk2000.com@gmail.com>
2025-04-17 00:39:20 +0530

    get post request

---
+++ include_file.php
@@ -0,0 +1,10 @@
+<?php
+include("./hello_world.php");//warning
+for($i=0;$i<10;$i++){
+    //include_once("./hello_world.php"");//it means that file is loaded once
+}
+//require(".//hello_world.php");//fatal error
+for($i=0;$i<10;$i++){
+    //require_once("./hello_world.php"");//it means
+}
+?>
---
+++ php_form.php/singup.php
@@ -0,0 +1,12 @@
+<?php
+if($_POST){
+    echo"user name is : ".$_POST['user_name'];
+    echo "<br/>";
+    echo"user email is : ".$_POST['user_email'];
+    echo "<br/>";
+
+    echo"user name is : ".$_POST['user_password'];
+
+
+}
+?>
