e71ff9fcbb3fc68be75f5c0a6b18621513f502be
shivam <shivamk2000.com@gmail.com>
2025-04-17 12:11:25 +0530

    cookies

---
+++ cookies.php
@@ -0,0 +1,19 @@
+<?php
+setcookie("fruits","apple",time()+(86400));
+setcookie("color","red",time()+(86400));
+
+if(isset($_COOKIE['fruit'])){
+    echo "current fruit is" .$_COOKIE['fruit'];
+}else{
+    echo "no fruit set";
+}
+echo "<br/>"
+
+if(isset($_COOKIE['color'])){
+    echo "current color is ".$_ $_COOKIE['color'];
+}else{
+    echo "no color set";
+}
+echo "<br/>";
+print_r($_COOKIE);
+?>
